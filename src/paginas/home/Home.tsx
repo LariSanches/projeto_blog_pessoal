02feb8e06845dfdeeacf9ddada5838dd99092cda
Larissa Sanches <larissaesanches@gmail.com>
2024-04-15 11:12:49 -0300

    CRUD tema

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -10,7 +10,7 @@ function Home() {
   return (
     <div className='flex justify-center items-center'>
       <div>
-        <h2 className="text-slate-900 text-5xl  my-4">Logar</h2>
+        <h2 className="text-slate-900 text-5xl  my-4">Bem vindo!</h2>
         <h2 className="text-slate-900 text-4xl ">Ola user : {nome}</h2>
         <Link to="/login" className="my-4 rounded bg-indigo-400
          hover:bg-indigo-900 text-white w-1/2 py-2 flex justify-center">
